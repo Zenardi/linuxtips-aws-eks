4753e1527f0f3d705c2cb9614def1d074ba97658
Eduardo Zenardi <du.zenardi@gmail.com>
2025-01-21 16:46:24 -0300

    fix variable names

--- README.md
+++ README.md
@@ -19,3 +19,8 @@ cd vanilla
 terraform plan -var-file="environment/prod/terraform.tfvars" -out eks.plan
 terraform apply eks.plan
 ```
+
+# Connect to EKS
+```shell
+aws eks update-kubeconfig --region us-east-1 --name linuxtips-cluster
+```
--- vanilla/data.tf
+++ vanilla/data.tf
@@ -12,7 +12,7 @@ data "aws_ssm_parameter" "private_subnets" {
   name  = var.ssm_private_subnets[count.index]
 }
 
-data "aws_ssm_parameter" "pods_subnets" {
+data "aws_ssm_parameter" "pod_subnets" {
   count = length(var.ssm_pod_subnets)
   name  = var.ssm_pod_subnets[count.index]
 }
