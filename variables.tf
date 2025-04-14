0c795b032cf553f5a1d40a1528c41884ce7ff1e4
Eduardo Zenardi <du.zenardi@gmail.com>
2025-04-14 16:08:46 -0300

    argo rollouts

--- variables.tf
+++ variables.tf
@@ -159,3 +159,17 @@ variable "keda_version" {
   default     = "2.16.0"
 
 }
+
+// Argo Rollouts
+
+variable "argo_rollouts_version" {
+  type        = string
+  default     = "2.34.1"
+  description = "value of argo rollouts version"
+}
+
+variable "argo_rollouts_host" {
+  type        = string
+  default     = "argo-rollouts.zenardi.com.br"
+  description = "Host do Argo Rollouts"
+}
